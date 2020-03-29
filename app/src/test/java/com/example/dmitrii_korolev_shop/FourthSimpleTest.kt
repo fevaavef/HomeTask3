b2c9860e31686ee10747231195d159f6034e4f0d
Dmitriy <dvk.09@mail.ru>
2020-03-29 12:24:19 +0300

    FourthSimpleTest created

---
+++ app/src/test/java/com/example/dmitrii_korolev_shop/FourthSimpleTest.kt
@@ -0,0 +1,12 @@
+package com.example.dmitrii_korolev_shop
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import kotlin.math.roundToInt
+
+class FourthSimpleTest {
+    @Test
+    fun addition_isCorrect() {
+    }
+}
