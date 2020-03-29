329b9b32fac3a9e21695f18f6d0821123c52e38d
Dmitriy <dvk.09@mail.ru>
2020-03-29 12:07:01 +0300

    FirsSimpleTest created

---
+++ app/src/test/java/com/example/dmitrii_korolev_shop/FirstSimpleTest.kt
@@ -0,0 +1,12 @@
+package com.example.dmitrii_korolev_shop
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import kotlin.math.roundToInt
+
+class FirstSimpleTest {
+    @Test
+    fun addition_isCorrect() {
+    }
+}
