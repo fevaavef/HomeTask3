1a40d309bec1eb34b61ab96bcb942f97427e1628
Dmitriy <dvk.09@mail.ru>
2020-03-29 11:22:25 +0300

    MathTest created

---
+++ app/src/test/java/com/example/dmitrii_korolev_shop/MathTest.kt
@@ -0,0 +1,12 @@
+package com.example.dmitrii_korolev_shop
+
+import org.junit.Test
+
+import org.junit.Assert.*
+import kotlin.math.roundToInt
+
+class MathTest {
+    @Test
+    fun addition_isCorrect() {
+    }
+}
